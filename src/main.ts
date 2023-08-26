89d2ed1536dcaa458d958e1cab187e9fffe5f3da
Sven Aarts <sven_aarts@epam.com>
2023-08-26 16:35:41 +0200

    remove title to fix build

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,4 @@ import App from './App.vue';
 import './css/reset.css';
 import router from './router';
 
-router.beforeEach((to, from, next) => {
-    // Update the page title based on the route's meta field
-    document.title = to.meta.title || 'Default Title';
-    next();
-});
 createApp(App).use(router).mount('#app');
